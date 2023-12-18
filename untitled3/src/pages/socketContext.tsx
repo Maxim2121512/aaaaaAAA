7913d326b74bf2248c6892c2c8a45f20c004a412
Maxim2121512 <90821934+Maxim2121512@users.noreply.github.com>
2023-12-18 12:48:00 +0300

    Add files via upload

---
+++ untitled3/src/pages/socketContext.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
+
+const SocketContext = createContext<Socket | undefined>(undefined);
+
+export const useSocket = () => {
+    const socket = useContext(SocketContext);
+    if (!socket) {
+        throw new Error('useSocket must be used within a SocketProvider');
+    }
+    return socket;
+};
+
+interface SocketProviderProps {
+    children: React.ReactNode;
+}
+
+export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+    const [socket, setSocket] = useState<Socket>();
+
+    useEffect(() => {
+        const newSocket = io('http://localhost:5001', { transports: ['websocket'] });
+        setSocket(newSocket);
+
+        return () => {
+            newSocket.close();
+        };
+    }, []);
+
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
